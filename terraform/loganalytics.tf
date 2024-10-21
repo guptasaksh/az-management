7fb207f43b03a56dba1bee16bcef12f34bcbf579
sakshi gupta <guptasaksh@microsoft.com>
2024-10-21 10:31:40 +0530

    add

--- terraform/loganalytics.tf
+++ terraform/loganalytics.tf
@@ -11,7 +11,7 @@ module "log_analytics_workspace" {
   enable_telemetry                          = var.enable_telemetry
   location                                  = azurerm_resource_group.rg.location
   resource_group_name                       = azurerm_resource_group.rg.name
-  name                                      = "var.loganalyticsname"
+  name                                      = "myworkspace"
   log_analytics_workspace_retention_in_days = 30
   log_analytics_workspace_sku               = "PerGB2018"
   log_analytics_workspace_identity = {
